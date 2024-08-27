996498a747fb7583546457311b69a4a58e646e91
justlikesh <lpsac@naver.com>
2024-08-27 09:21:37 +0900

    Fix : 불필요한 로직 제거
    - Controller 로 데이터를 넘겨줄때 Banner 도메인을 제외한 도메인은 ItemRepository 클래스를 생성자로 만들어서 ( 스프링에 빈주입 ) Controller 에서 응답과 반환

--- src/main/java/ASAC8/_SpringPractice/weekend/repository/Repository.java
+++
@@ -1,9 +0,0 @@
-package ASAC8._SpringPractice.weekend.repository;
-
-import ASAC8._SpringPractice.item.Item;
-
-import java.util.List;
-
-public interface Repository {
-    public abstract List<Item> getAllItems();
-}
