855ec154b05cdf9b11fa74fa3dfb16f06a369850
justlikesh <lpsac@naver.com>
2024-08-29 11:09:09 +0900

    Feat : 메인 페이지 API 구현 을 위한 Service 로직 구현
    - 가독성을 위해 CRUD 순서를 맞추기위해 getMain 메서드와 createMain 메서드의 순서를 변경
    - id , title, imageUrl 을 입력하면 BannerItemRepository 에서 해당 데이터가 변경되는 updateMain 메서드 구현
    - id 값을 입력하면 BannerItemRepository 에서 해당 id 값에 해당되는 데이터만 삭제하는 deleteMain 메서드 구현
    - BannerItemRepository 에서 모든 데이터를 삭제하고 그것을 리스트 형태의 BannerItem 에 넣어주고 stream.map 을 사용하여 MainItem 리스트 타입의 response 로 변환해주고 MainItemsResponseDto 의 정적 팩토리 메서드를 통해 반환한다

--- src/main/java/ASAC8/_SpringPractice/main/service/MainService.java
+++ src/main/java/ASAC8/_SpringPractice/main/service/MainService.java
@@ -4,7 +4,6 @@ import ASAC8._SpringPractice.item.*;
 import ASAC8._SpringPractice.main.controller.response.MainItemsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -14,22 +13,41 @@ public class MainService {
 
     private final BannerItemRepository bannerItemRepository;
 
+    public MainItemsResponseDto createMain (BannerItem bannerItem) {
+        bannerItemRepository.addBannerItem(bannerItem);
+        List<BannerItem> addBannerItems = bannerItemRepository.getAllItems();
+        List<MainItem> response = addBannerItems.stream()
+                .map(MainItem::of)
+                .toList();
+
+        return MainItemsResponseDto.of(response);
+    }
+
     public MainItemsResponseDto getMain(){
         List<BannerItem> banneritems = bannerItemRepository.getAllItems();
         List<MainItem> response = banneritems.stream()
                 .map(MainItem::of)
                 .toList();
 
-//        MainItemsResponseDto mainItemsResponseDto = MainItemsResponseDto.of(response);
-//
-//        return mainItemsResponseDto;
         return MainItemsResponseDto.of(response);
     }
 
-    public MainItemsResponseDto createMain (BannerItem bannerItem) {
-        bannerItemRepository.addBannerItem(bannerItem);
-        List<BannerItem> updatedBannerItems = bannerItemRepository.getAllItems();
-        List<MainItem> response = updatedBannerItems.stream()
+    public BannerItem updateMain (Integer id, String title, String imageUrl) {
+        BannerItem updateBannerItem = bannerItemRepository.updateBannerItem(id, title, imageUrl);
+
+        return updateBannerItem;
+    }
+
+    public BannerItem deleteMain (Integer id) {
+        BannerItem deleteBannerItem = bannerItemRepository.deleteBannerItem(id);
+
+        return deleteBannerItem;
+    }
+
+    public MainItemsResponseDto clearMain () {
+        bannerItemRepository.deleteAllBannerItem();
+        List<BannerItem> clearBannerItems = bannerItemRepository.getAllItems();
+        List<MainItem> response = clearBannerItems.stream()
                 .map(MainItem::of)
                 .toList();
 
