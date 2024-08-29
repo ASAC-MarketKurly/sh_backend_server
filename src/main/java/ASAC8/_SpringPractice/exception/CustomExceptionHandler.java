726c46c774c4023dfb34b05215a6d3d6bcd9144f
justlikesh <lpsac@naver.com>
2024-08-29 11:12:59 +0900

    Feat : CustomExceptionHandler 에서 Exception 추가
    - id 값이 1부터 시작인데 0입력시 에러처리를 위한 idErrorException 구현
    - 정의한 NullPointerException, idErrorException 을 제외한 모든 에러 발생시 RuntimeException 로 처리하기위한 로직 구현

--- src/main/java/ASAC8/_SpringPractice/exception/CustomExceptionHandler.java
+++ src/main/java/ASAC8/_SpringPractice/exception/CustomExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 // 역할과 책임의 분리 : 책임분리
 // - CustomExceptionHandler : 에러 중앙 처리
 // - NewProductItemsController : 고객 반응 신상품 반환등 API 제공
+//
 
 @Slf4j              // Slf4j + log.??? 한세트라고 생각하기
 @ControllerAdvice   //Controller 앞단에 있는 컨트롤러를 도와주는 애들
@@ -22,11 +23,19 @@ public class CustomExceptionHandler {
                 .body(ExceptionError.of(5, "NULL 에러가 발생했습니다 "));
     }
 
-//    @ExceptionHandler(value = RuntimeException.class)  // 어떤 종류의 exception 을 처리할 것인가
-//    public ResponseEntity<ExceptionError /* Generic 제네릭 : 어떤 클래스도 가능*/ > catchRuntimeException(RuntimeException e){    // null 은 void 타입
-//        log.error("처음으로 우리가 모르는 에러가 발생했습니다", e.fillInStackTrace());     // try catch 에서 catch 에 있는 로직을 쓴다
-//        return ResponseEntity
-//                .status(500)
-//                .body(ExceptionError.of(1, "서버팀에서 보고 있습니다"));  // 메세지 종류 1. 솔직하게 말하기 (서버가 망가지고있습니다)
-//    }                                                                                //     2. 숨기는거      (서버가 점검중입니다)
+    @ExceptionHandler(value = AaronException.class)
+    public ResponseEntity<ExceptionError> idErrorException (AaronException e){
+        log.info("id 값에 0이 입력되는 에러가 발생했습니다.", e.fillInStackTrace());
+        return ResponseEntity
+                .status(400)
+                .body(ExceptionError.of(2,"id 값을 다시 확인해주세요"));
+    }
+
+    @ExceptionHandler(value = RuntimeException.class)  // 어떤 종류의 exception 을 처리할 것인가
+    public ResponseEntity<ExceptionError /* Generic 제네릭 : 어떤 클래스도 가능*/ > catchRuntimeException(RuntimeException e){    // null 은 void 타입
+        log.error("처음으로 우리가 모르는 에러가 발생했습니다", e.fillInStackTrace());     // try catch 에서 catch 에 있는 로직을 쓴다
+        return ResponseEntity
+                .status(500)
+                .body(ExceptionError.of(1, "서버팀에서 보고 있습니다"));  // 메세지 종류 1. 솔직하게 말하기 (서버가 망가지고있습니다)
+    }                                                                                //     2. 숨기는거      (서버가 점검중입니다)
 }
